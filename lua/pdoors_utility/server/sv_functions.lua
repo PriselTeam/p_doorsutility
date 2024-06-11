8cfc683b7b7fcc1d09b612bf1794b51398243dd0
Ekali <pcnlmmm@gmail.com>
2024-06-11 15:42:40 +0200

    (feat) Price with house added.

--- lua/pdoors_utility/server/sv_functions.lua
+++ lua/pdoors_utility/server/sv_functions.lua
@@ -2,11 +2,11 @@ Prisel.DoorsUtility.Zones = Prisel.DoorsUtility.Zones or {}
 
 function Prisel.DoorsUtility:Initialize()
     if not sql.TableExists("prisel_doorsutility") then
-        sql.Query("CREATE TABLE prisel_doorsutility (id INTEGER PRIMARY KEY AUTOINCREMENT, start_pos TEXT, end_pos TEXT, doors TEXT)")
+        sql.Query("CREATE TABLE prisel_doorsutility (id INTEGER PRIMARY KEY AUTOINCREMENT, start_pos TEXT, end_pos TEXT, doors TEXT, price INT)")
     end
 end
 
-function Prisel.DoorsUtility:NewZone(start, endpos, doors)
+function Prisel.DoorsUtility:NewZone(start, endpos, doors, price)
     start = {x = start.x, y = start.y, z = start.z}
     endpos = {x = endpos.x, y = endpos.y, z = endpos.z}
     
@@ -18,11 +18,12 @@ function Prisel.DoorsUtility:NewZone(start, endpos, doors)
         Start = start,
         End = endpos,
         Doors = doors,
+        Price = price
     }
 
     table.insert(Prisel.DoorsUtility.Zones, zone)
 
-    local query = ("INSERT INTO prisel_doorsutility (start_pos, end_pos, doors) VALUES ('%s', '%s', '%s')"):format(util.TableToJSON(start), util.TableToJSON(endpos), util.TableToJSON(doors))
+    local query = ("INSERT INTO prisel_doorsutility (start_pos, end_pos, doors, price) VALUES ('%s', '%s', '%s', %i)"):format(util.TableToJSON(start), util.TableToJSON(endpos), util.TableToJSON(doors), price)
     sql.Query(query)
 end
 
@@ -35,11 +36,15 @@ function Prisel.DoorsUtility:LoadDoors()
         local start = util.JSONToTable(v.start_pos)
         local endpos = util.JSONToTable(v.end_pos)
         local doors = util.JSONToTable(v.doors)
+        local price = v.price
+
+        print(start, endpos, doors, price)
 
         Prisel.DoorsUtility.Zones[k] = {
             Start = start,
             End = endpos,
             Doors = doors,
+            Price = price
         }
     end
 end
@@ -50,6 +55,7 @@ function Prisel.DoorsUtility:GetOtherDoors(ent)
         for u, i in ipairs(v.Doors) do
             if Entity(i) == ent then
                 doors = v.Doors
+                doors.price = v.Price
             end
         end
     end
@@ -60,6 +66,12 @@ end
 function Prisel.DoorsUtility:playerBuyDoor(ply, ent)
     local doors = Prisel.DoorsUtility:GetOtherDoors(ent)
 
+    if not ply:canAfford(doors.price) then
+        DarkRP.notify(ply, 1, 4, "Vous n'avez pas assez d'argent pour acheter cette maison. [Prix: " .. doors.price .. "€]")
+    return end
+    
+    ply:addMoney(-doors.price)
+
     for k, v in ipairs(doors) do
         local vent = Entity(v)
         if vent:PRIsDoor() then
@@ -68,11 +80,16 @@ function Prisel.DoorsUtility:playerBuyDoor(ply, ent)
             vent:keysOwn(ply)
         end
     end
+
+    DarkRP.notify(ply, 0, 4, "Vous avez acheté cette maison pour " .. doors.price .. "€")
 end
 
 function Prisel.DoorsUtility:playerSellDoor(ply, ent)
     local doors = Prisel.DoorsUtility:GetOtherDoors(ent)
 
+    local price = math.floor(doors.price / 2)
+
+    
     for k, v in ipairs(doors) do
         local vent = Entity(v)
         if vent:PRIsDoor() then
@@ -81,4 +98,7 @@ function Prisel.DoorsUtility:playerSellDoor(ply, ent)
             vent:keysUnOwn(ply)
         end
     end
+    
+    ply:addMoney(price)
+    DarkRP.notify(ply, 0, 4, "Vous avez vendu cette maison pour " .. price .. "€")
 end
